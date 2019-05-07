0c696aae5528453df9c75e24d0661a4cf08a9ee6
wene <354007048@qq.com>
2019-05-07 18:13:09 +0800

    init

--- application/api/controller/v1/Book.php
+++ application/api/controller/v1/Book.php
@@ -5,17 +5,22 @@
  * Date: 2019/4/20
  * Time: 19:57
  */
-
 namespace app\api\controller\v1;
-
 use app\api\model\Book as BookModel;
 use think\Request;
-
+/**
+ * Class Book
+ * @route('v1/book')
+ * @middleware('linRouteParam')
+ * @package app\api\controller\v1
+ */
 class Book
 {
     /**
      * 查询指定bid的图书
-     * @param $bid
+     * @route(':bid','get')
+     * @param Request $bid
+     * @param('bid','bid的图书','require')
      * @return mixed
      */
     public function getBook($bid)
@@ -23,9 +28,10 @@ class Book
         $result = BookModel::get($bid);
         return $result;
     }
-
     /**
      * 查询所有图书
+     * @route('','get')
+     * @validate('LoginForm')
      * @return mixed
      */
     public function getBooks()
@@ -33,18 +39,19 @@ class Book
         $result = BookModel::all();
         return $result;
     }
-
     /**
      * 搜索图书
      */
     public function search()
     {
-
     }
-
     /**
      * 新建图书
+     * @route('','post')
      * @param Request $request
+     * @param('title','图书名称','require')
+     * @param('author','图书作者','require')
+     * @param('image','图书img','require')
      * @return \think\response\Json
      */
     public function create(Request $request)
@@ -53,7 +60,16 @@ class Book
         BookModel::create($params);
         return writeJson(201, '', '新建图书成功');
     }
-
+    /**
+     * 更新图书
+     * @route(':id','put')
+     * @param Request $request
+     * @param('id','图书ID','require')
+     * @param('title','图书名称','require')
+     * @param('author','图书作者','require')
+     * @param('image','图书img','require')
+     * @return \think\response\Json
+     */
     public function update(Request $request)
     {
         $params = $request->put();
@@ -61,10 +77,10 @@ class Book
         $bookModel->save($params, ['id' => $params['id']]);
         return writeJson(201, '', '更新图书成功');
     }
-
     /**
      * @auth('删除图书','图书')
-     * @param $bid
+     * @route(':bid','delete')
+     * @param('id','图书ID','require')
      * @return \think\response\Json
      * @throws \app\lib\exception\token\TokenException
      * @throws \think\Exception
@@ -72,7 +88,6 @@ class Book
     public function delete($bid)
     {
         BookModel::destroy($bid);
-
         logger('删除了id为' . $bid . '的图书');
         return writeJson(201, '', '删除图书成功');
     }
